124f78eb3f4941ed492d11cf8864f7f0e6453368
Matt <mattosullivan06@gmail.com>
2024-03-14 11:04:55 -0700

    Create link for new ticket icon

--- app/(components)/NavigationBar.tsx
+++ app/(components)/NavigationBar.tsx
@@ -8,11 +8,11 @@ const NavigationBar = () => {
     <nav className="flex justify-between bg-sky-500 p-4">
       <div className="flex items-center space-x-4 text-white">
         <Link href="/">
-          <FontAwesomeIcon icon={faHome} className="icon" />
+          <FontAwesomeIcon icon={faHome}/>
         </Link>
-        <div>
-          <FontAwesomeIcon icon={faTicket} className="icon" />
-        </div>
+        <Link href="/Ticket/new">
+          <FontAwesomeIcon icon={faTicket}/>
+        </Link>
       </div>
       <div className="text-white">
         <p className="text-default-text">Username/email?</p>
