4a34f7bd65c3f3f22257cb93e7afe53baa9ff638
Matt <mattosullivan06@gmail.com>
2024-03-14 21:41:48 -0700

    schema change

--- app/api/ticket/route.ts
+++ app/api/ticket/route.ts
@@ -7,6 +7,9 @@ export async function POST(request: Request) {
   const body = await request.json();
   const ticket = await db.ticket.create({
     data: {
+      firstName: body.firstName,
+      lastName: body.lastName,
+      status: body.status,
       email: body.email,
       title: body.title,
       description: body.description,
