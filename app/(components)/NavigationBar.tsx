9f37043ce4d91d90823fa79521b44f61e38b20a9
Matt <mattosullivan06@gmail.com>
2024-03-15 09:40:56 -0700

    fix styling

--- app/(components)/NavigationBar.tsx
+++ app/(components)/NavigationBar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const NavigationBar: React.FC = () => {
   return (
-    <nav className="flex justify-between bg-sky-500 p-4">
+    <nav className="flex justify-between bg-indigo-600 p-4">
       <div className="flex items-center space-x-4 text-white">
         <Link href="/">
           <FontAwesomeIcon icon={faHome} />
@@ -13,7 +13,10 @@ const NavigationBar: React.FC = () => {
       </div>
       <div className="text-white">
         <Link href="/AdminPanel">
-          <p className="text-default-text">Admin Panel</p>
+          <button className="flex items-center bg-transparent border-none text-default-text cursor-pointer">
+            <FontAwesomeIcon icon={faUser} className="mr-2" />
+            Admin Panel
+          </button>
         </Link>
       </div>
     </nav>
--- app/(components)/TicketCard.tsx
+++ app/(components)/TicketCard.tsx
@@ -14,7 +14,7 @@ const TicketCard: React.FC<TicketCardProps> = (props: TicketCardProps) => {
       <div>{props.ticket.title}</div>
       <div>{props.ticket.status}</div>
       <Link href={`/Ticket/${props.ticket.id}`}>
-        <button className="bg-sky-500 text-white rounded-lg px-4 py-2 hover:bg-sky-600 transition-colors duration-300 ease-in-out">
+        <button className="bg-indigo-600 text-white rounded-lg px-4 py-2 hover:bg-indigo-700 transition-colors duration-300 ease-in-out">
           View Ticket
         </button>
       </Link>
