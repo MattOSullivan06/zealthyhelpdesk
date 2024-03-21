fe0ebb02efc4f73135a9b7ab01fd84d9b9f4b543
Matt <mattosullivan06@gmail.com>
2024-03-21 12:01:51 -0700

    ticket form does not redirect to admin page, ticket form clears after submission, added success or failure model

--- app/(components)/TicketForm.tsx
+++ app/(components)/TicketForm.tsx
@@ -1,9 +1,7 @@
 "use client";
 import React, { useState, FormEvent } from "react";
-import { useRouter } from "next/navigation";
 
 const TicketForm: React.FC = () => {
-  const router = useRouter();
 
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
@@ -11,6 +9,8 @@ const TicketForm: React.FC = () => {
   const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [modalMessage, setModalMessage] = useState<string>("");
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,103 +25,133 @@ const TicketForm: React.FC = () => {
       status: "New",
     };
 
-    const res = await fetch(`/api/ticket`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTicket),
-    });
+    try {
+      const res = await fetch(`/api/ticket`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTicket),
+      });
 
-    if (res.status === 200) {
-      router.push("/AdminPanel");
-      router.refresh();
+      if (res.status === 200) {
+        setModalMessage("Your Ticket Has Been Received!");
+        setModalVisible(true);
+        resetForm();
+      } else {
+        setModalMessage("Submission failed. Please try again.");
+        setModalVisible(true);
+      }
+    } catch (error) {
+      setModalMessage("An error occurred. Please try again.");
+      setModalVisible(true);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const resetForm = () => {
+    setTitle("");
+    setDescription("");
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white-500">
+    <div className="flex justify-center items-center min-h-screen bg-white">
       <form
         onSubmit={handleSubmit}
-        className="w-full md:w-2/3 max-w-xl bg-gray-200 p-6 rounded-lg shadow-md mt-24 mb-24"
+        className="w-full md:w-2/3 max-w-xl bg-gray-200 p-6 rounded-lg shadow-md my-8"
       >
         <h1 className="text-3xl font-bold mb-6 text-center">
           HELP DESK SUBMISSION FORM
         </h1>
         <div className="bg-gray-200 p-4 rounded-lg mb-4">
-          <div className="flex flex-col space-y-4 mb-4">
-            <div>
-              <label className="block text-lg">
-                <span className="text-gray-700">First Name</span>
-                <span className="text-red-500">*</span>
-                <input
-                  required
-                  type="text"
-                  onChange={(e) => setFirstName(e.target.value)}
-                  value={firstName}
-                  className="mt-1 block w-full border border-black rounded-md shadow-sm"
-                />
-              </label>
-            </div>
-            <div>
-              <label className="block text-lg">
-                <span className="text-gray-700">Last Name</span>
-                <span className="text-red-500">*</span>
-                <input
-                  required
-                  type="text"
-                  onChange={(e) => setLastName(e.target.value)}
-                  value={lastName}
-                  className="mt-1 block w-full border border-black rounded-md shadow-sm"
-                />
-              </label>
-            </div>
-          </div>
-          <div>
-            <label className="block mb-4 text-lg">
-              <span className="text-gray-700">Email</span>
-              <span className="text-red-500">*</span>
-              <input
-                required
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                className="mt-1 block w-full border border-black rounded-md shadow-sm"
-              />
-            </label>
-          </div>
-          <div>
-            <label className="block mb-4 text-lg">
-              <span className="text-gray-700">Title</span>
-              <span className="text-red-500">*</span>
-              <input
-                required
-                type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                className="mt-1 block w-full border border-black rounded-md shadow-sm"
-              />
-            </label>
-          </div>
-          <div>
-            <label className="block mb-4 text-lg">
-              <span className="text-gray-700">Description</span>
-              <span className="text-red-500">*</span>
-              <textarea
-                required
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                className="mt-1 block w-full border border-black rounded-md shadow-sm h-48"
-              />
-            </label>
-          </div>
+          <label className="block mb-4 text-lg">
+            <span className="text-gray-700">First Name</span>
+            <span className="text-red-500">*</span>
+            <input
+              required
+              type="text"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+              className="mt-1 block w-full border border-black rounded-md shadow-sm"
+            />
+          </label>
+
+          <label className="block mb-4 text-lg">
+            <span className="text-gray-700">Last Name</span>
+            <span className="text-red-500">*</span>
+            <input
+              required
+              type="text"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+              className="mt-1 block w-full border border-black rounded-md shadow-sm"
+            />
+          </label>
+
+          <label className="block mb-4 text-lg">
+            <span className="text-gray-700">Email</span>
+            <span className="text-red-500">*</span>
+            <input
+              required
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="mt-1 block w-full border border-black rounded-md shadow-sm"
+            />
+          </label>
+
+          <label className="block mb-4 text-lg">
+            <span className="text-gray-700">Title</span>
+            <span className="text-red-500">*</span>
+            <input
+              required
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              className="mt-1 block w-full border border-black rounded-md shadow-sm"
+            />
+          </label>
+
+          <label className="block mb-4 text-lg">
+            <span className="text-gray-700">Description</span>
+            <span className="text-red-500">*</span>
+            <textarea
+              required
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              className="mt-1 block w-full border border-black rounded-md shadow-sm h-48"
+            />
+          </label>
+
           <button
             type="submit"
-            className="w-full px-4 py-4 text-white bg-indigo-600 rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700 mt-8" // Adjusted margin
+            className="w-full px-4 py-4 text-white bg-indigo-600 rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700 mt-8"
             disabled={isLoading}
           >
             {isLoading ? "Submitting..." : "Submit Ticket"}
           </button>
         </div>
       </form>
+
+      {modalVisible && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <p className="text-lg">{modalMessage}</p>
+            <button
+              onClick={closeModal}
+              className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
