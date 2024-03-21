2f4a724cf2f1361811f5a4e1d07d5789c271ba5d
Matt <mattosullivan06@gmail.com>
2024-03-21 11:38:03 -0700

    make ticket form mobile and tablet responsive

--- app/(components)/TicketForm.tsx
+++ app/(components)/TicketForm.tsx
@@ -41,14 +41,14 @@ const TicketForm: React.FC = () => {
     <div className="flex justify-center items-center min-h-screen bg-white-500">
       <form
         onSubmit={handleSubmit}
-        className="w-2/3 max-w-xl bg-gray-200 p-6 rounded-lg shadow-md mt-24 mb-24"
+        className="w-full md:w-2/3 max-w-xl bg-gray-200 p-6 rounded-lg shadow-md mt-24 mb-24"
       >
         <h1 className="text-3xl font-bold mb-6 text-center">
           HELP DESK SUBMISSION FORM
         </h1>
         <div className="bg-gray-200 p-4 rounded-lg mb-4">
-          <div className="flex space-x-4 mb-4">
-            <div className="w-1/2 relative">
+          <div className="flex flex-col space-y-4 mb-4">
+            <div>
               <label className="block text-lg">
                 <span className="text-gray-700">First Name</span>
                 <span className="text-red-500">*</span>
@@ -61,7 +61,7 @@ const TicketForm: React.FC = () => {
                 />
               </label>
             </div>
-            <div className="w-1/2 relative">
+            <div>
               <label className="block text-lg">
                 <span className="text-gray-700">Last Name</span>
                 <span className="text-red-500">*</span>
@@ -75,38 +75,44 @@ const TicketForm: React.FC = () => {
               </label>
             </div>
           </div>
-          <label className="block mb-4 text-lg">
-            <span className="text-gray-700">Email</span>
-            <span className="text-red-500">*</span>
-            <input
-              required
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className="mt-1 block w-full border border-black rounded-md shadow-sm"
-            />
-          </label>
-          <label className="block mb-4 text-lg">
-            <span className="text-gray-700">Title</span>
-            <span className="text-red-500">*</span>
-            <input
-              required
-              type="text"
-              onChange={(e) => setTitle(e.target.value)}
-              value={title}
-              className="mt-1 block w-full border border-black rounded-md shadow-sm"
-            />
-          </label>
-          <label className="block mb-4 text-lg">
-            <span className="text-gray-700">Description</span>
-            <span className="text-red-500">*</span>
-            <textarea
-              required
-              onChange={(e) => setDescription(e.target.value)}
-              value={description}
-              className="mt-1 block w-full border border-black rounded-md shadow-sm h-48"
-            />
-          </label>
+          <div>
+            <label className="block mb-4 text-lg">
+              <span className="text-gray-700">Email</span>
+              <span className="text-red-500">*</span>
+              <input
+                required
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                className="mt-1 block w-full border border-black rounded-md shadow-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="block mb-4 text-lg">
+              <span className="text-gray-700">Title</span>
+              <span className="text-red-500">*</span>
+              <input
+                required
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                className="mt-1 block w-full border border-black rounded-md shadow-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="block mb-4 text-lg">
+              <span className="text-gray-700">Description</span>
+              <span className="text-red-500">*</span>
+              <textarea
+                required
+                onChange={(e) => setDescription(e.target.value)}
+                value={description}
+                className="mt-1 block w-full border border-black rounded-md shadow-sm h-48"
+              />
+            </label>
+          </div>
           <button
             type="submit"
             className="w-full px-4 py-4 text-white bg-indigo-600 rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700 mt-8" // Adjusted margin
